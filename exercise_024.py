6643f152f5d3556948f27ee4ed2d94d41e6af001
Br_Quinones <dadidhuisa@gmail.com>
2025-09-15 20:43:45 -0500

    exercise_024

---
+++ exercise_024.py
@@ -0,0 +1,16 @@
+# Escribe un programa que imprima los 50 primeros números de la sucesión
+# de Fibonacci empezando en 0.
+# - La serie Fibonacci se compone por una sucesión de números en
+#   la que el siguiente siempre es la suma de los dos anteriores.
+#   0, 1, 1, 2, 3, 5, 8, 13...
+
+def fibonacci_series():
+    a = 0 ; b = 1   
+    print(a) ; print(b)
+    for x in range(50):
+        print(a+b)
+        a,b = b,a+b
+
+fibonacci_series()
+
+#24:39 minutos
