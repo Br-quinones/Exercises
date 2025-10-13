34fc053f884628b9c93191aa74e66f9eb655f686
Br_Quinones <dadidhuisa@gmail.com>
2025-10-12 19:24:18 -0500

    exercise_048

--- exercise_048.py
+++ exercise_048.py
@@ -2,7 +2,11 @@
 # Que se pueda sumar y restar.
 # Se prohibe librerias o funciones que lo hagan directamente.
 
-def add_subtract_matrix(*tuple_matrixs):
+def add_subtract_matrix(sign,*tuple_matrixs):
+    #Comprobacion de signo (Verificar si es negativo o positivo)
+    if sign != "+" and sign != "-":
+        raise ValueError("Signo no valido")
+    
     #Comprobacion de tamaño (Verificar si son iguales las matricez)
     amount_of_rows    = []
     amount_of_columns = []
@@ -13,25 +17,29 @@ def add_subtract_matrix(*tuple_matrixs):
             amount_of_columns.append(len(rows))
 
     if len(set(amount_of_columns)) != 1:
-        raise "Error. Matricez de diferente tamaño el columnas"
+        raise ValueError("Error. Matricez de diferente tamaño el columnas")
     if len(set(amount_of_rows)) != 1:
-        raise "Error. Matricez de diferente tamaño en filas"
+        raise ValueError("Error. Matricez de diferente tamaño en filas")
 
-    #Crear numero de filas en matris a retornar
+    # Crear matris a retornar
     matrix_to_return = []
-
-    for number in range(amount_of_rows[0]):
+    
+    # Agregar filas y columnas
+    for position,_ in enumerate(range(amount_of_rows[0])):
         matrix_to_return.append([])
+        for _ in range(amount_of_columns[0]):
+            matrix_to_return[position].append(0)
 
-    #Restar o Sumar matricez
     for matrix in tuple_matrixs:
         for position_01,row in enumerate(matrix):
-            for position_02,number in enumerate(matrix):
-                matrix_to_return[position_01] += number
+            for position_02,number in enumerate(row):
+                if sign == "+":
+                    matrix_to_return[position_01][position_02] += number
+                if sign == "-":
+                    matrix_to_return[position_01][position_02] -= number
+            
+    return matrix_to_return
 
-    print(matrix_to_return)
-
-        
 ######################
 user_matrix_01 = [[3,5,4],
                 [-4,3,5],]
@@ -42,8 +50,17 @@ user_matrix_02 = [[3,9,-5],
 user_matrix_03 = [[1,-1,4],
                 [9,-6,-10]]
 
-add_subtract_matrix(user_matrix_01,user_matrix_02,user_matrix_03)
+print("-------- Matrices --------")
+print(user_matrix_01)
+print(user_matrix_02)
+print(user_matrix_03)
+print("--------------------------")
+print("--- Suma de 3 matrices ---")
+print(add_subtract_matrix("+",user_matrix_01,user_matrix_02,user_matrix_03))
+print("--- Resta de 3 matrices ---")
+print(add_subtract_matrix("-",user_matrix_01,user_matrix_02,user_matrix_03))
+print("--------------------------")
 
-#01:07:50
+# 93 minutos
 
-#Se continuara mañana
+#Dios mio mi señor jesucristo me ha abandonado. Me duele todo mi cuerpo y tengos mareos a mas no poder. Esase esta la razón de hacer con tanta lentitud los ejercicios de matricez.
